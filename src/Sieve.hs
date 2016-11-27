cfa14333febb7ad587db0bcf90455e97b63dfa59
Charlie Harvey <charlie@mallory.hendred.net>
2016-11-27 13:02:00 +0000

    Proper implementation of Sieve
    With thanks to @warundeclared.

--- src/Sieve.hs
+++ src/Sieve.hs
@@ -1,10 +1,17 @@
 module Sieve (primesUpTo) where
+import Data.List.Ordered (minus)
 
 primesUpTo :: Integer -> [Integer]
 primesUpTo n =
-    sieve [2..n]
+    sieve (2 : [3,5..n])
 
+-- with thanks to @warundeclared a more efficient actual sieve.
 sieve :: [Integer] -> [Integer]
 sieve [] = []
 sieve (x:xs) =
-    x : sieve (filter ((/=0) . (`mod`x)) xs)
+    x : sieve (xs `minus` [x^2, x^2+x..])
+
+sieveNaive :: [Integer] -> [Integer]
+sieveNaive [] = []
+sieveNaive (x:xs) =
+    x : sieveNaive (filter ((/=0).(`mod`x)) xs)
